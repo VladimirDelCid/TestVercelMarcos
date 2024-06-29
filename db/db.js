a93e55a5d371f6cb5519c74aed1c76f10588192e
VladimirDelCid <vladimir.delcid@bue.edu.ar>
2024-06-29 14:27:22 -0300

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const mySql = require('mysql2');
+
+const connection = mySql.createConnection(
+    {
+        /*
+        host : 'localhost',
+        user: 'root',
+        password : 'Loveisall77',
+        database: 'usuarios_db'
+        */
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+
+
+    connection.connect((err) => 
+    {
+        if(err)
+        {
+            console.error("ERROR conectando a la base de datos", err);
+            return;
+        }
+
+        console.log("Conectado EXITOSAMENTE a la base de datos");
+
+    });
+
+
+module.exports = connection;
